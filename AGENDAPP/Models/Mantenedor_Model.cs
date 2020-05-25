39b690e769848dc150be3ae7ce4cae930c75b9c6
Bastian <ba.moyano.del.pino@gmail.com>
2020-05-25 11:23:47 -0400

    arreglado

---
+++ AGENDAPP/Models/Mantenedor_Model.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AGENDAPP.Models
+{
+    public class Mantenedor_Model
+    {
+        public static string Formato = "yyyy/MM/dd";
+
+        #region Etiqueta
+        public static object Etiqueta()
+        {
+            using (MPS_DB db = new MPS_DB())
+            {
+                object[] datos = (from c in db.MPS_ETIQUETAS
+                                  select new
+                                  {
+                                      c.ID_ETIQUETA,
+                                      c.NOMBRE_COLORR,
+                                      c.COLOR,
+                                      c.FECHA_CREACION,
+                                      c.FECHA_EDICION,
+                                      c.ESTADO
+                                  }).ToArray();
+
+
+
+                return new { RESPUESTA = true, TIPO = 0, datos };
+
+            }
+        }
+
+        public static object GuardarEtiqueta(string NombreColor, string Color)
+        {
+            try
+            {
+                MPS_ETIQUETAS Mps_COLORES = new MPS_ETIQUETAS();
+                Mps_COLORES.NOMBRE_COLORR = NombreColor;
+                Mps_COLORES.COLOR = Color;
+                Mps_COLORES.FECHA_CREACION = DateTime.Now;
+                Mps_COLORES.ESTADO = 1;
+                using (MPS_DB db = new MPS_DB())
+                {
+                    db.MPS_ETIQUETAS.Add(Mps_COLORES);
+                    db.SaveChanges();
+
+                    object datos = (from c in db.MPS_ETIQUETAS
+                                    where c.ID_ETIQUETA == Mps_COLORES.ID_ETIQUETA
+                                    select new
+                                    {
+                                        c.ID_ETIQUETA,
+                                        c.NOMBRE_COLORR,
+                                        c.COLOR,
+                                        c.FECHA_CREACION,
+                                        c.FECHA_EDICION,
+                                        c.ESTADO
+                                    }).FirstOrDefault();
+
+                    return new { RESPUESTA = true, TIPO = 0, data = datos };
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new { RESPUESTA = false, TIPO = 1, e.Message };
+            }
+
+        }
+
+        public static object ListaEtiquetasId(int id)
+        {
+            using (MPS_DB db = new MPS_DB())
+            {
+
+
+
+                object[] datos = (from c in db.MPS_ETIQUETAS
+
+                                  where c.ID_ETIQUETA == id
+                                  select new
+                                  {
+                                      c.ID_ETIQUETA,
+                                      c.NOMBRE_COLORR,
+                                      c.COLOR,
+                                      c.FECHA_CREACION,
+                                      c.FECHA_EDICION,
+                                      c.ESTADO
+
+                                  }).ToArray();
+
+
+
+
+                return new { RESPUESTA = true, TIPO = 0, datos };
+            }
+        }
+
+        public static object ModificarEtiqueta(int ID_COLOR, string NOMBRE, string COLOR, int COD_ESTADO)
+        {
+            try
+            {
+                using (MPS_DB db = new MPS_DB())
+                {
+                    DateTime Fecha = DateTime.Now;
+                    string subQuery = $@"UPDATE MPS_ETIQUETAS SET NOMBRE_COLORR = '{ NOMBRE }' , COLOR = '{COLOR}' ,FECHA_EDICION='{ Fecha.ToString(Formato) }',
+                                       ESTADO = {COD_ESTADO} where ID_ETIQUETA = { ID_COLOR } ";
+                    db.Database.ExecuteSqlCommand(subQuery);
+
+                    object datos = (from c in db.MPS_ETIQUETAS
+                                    where c.ID_ETIQUETA == ID_COLOR
+                                    select new
+                                    {
+                                        c.ID_ETIQUETA,
+                                        c.NOMBRE_COLORR,
+                                        c.COLOR,
+                                        c.FECHA_CREACION,
+                                        c.FECHA_EDICION,
+                                        c.ESTADO
+                                    }).FirstOrDefault();
+
+
+
+                    return new { RESPUESTA = true, TIPO = 0, data = datos };
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new { RESPUESTA = false, TIPO = 1, e.Message };
+            }
+
+        }
+        #endregion
+
+        #region Ocupacion
+        public static object OcupacionesLitas()
+        {
+            using (MPS_DB db = new MPS_DB())
+            {
+                object[] datos = (from c in db.MPS_OCUPACIONES
+                                  select new
+                                  {
+                                      c.ID_OCUPACION,
+                                      c.NOMBRE_OCUPACION,
+                                      c.DESCRIPCION,
+                                      c.FECHA_CREACION,
+                                      c.FECHA_EDICION,
+                                      c.ESTADO
+                             
+                                  }).ToArray();
+
+
+
+                return new { RESPUESTA = true, TIPO = 0, datos };
+
+            }
+        }
+
+        public static object GuardarOcupaciones(string NombreOcupacion, string Descripcion)
+        {
+            try
+            {
+                MPS_OCUPACIONES Mps_OCUPACIONES = new MPS_OCUPACIONES();
+                Mps_OCUPACIONES.NOMBRE_OCUPACION = NombreOcupacion;
+                Mps_OCUPACIONES.DESCRIPCION = Descripcion;
+                Mps_OCUPACIONES.FECHA_CREACION = DateTime.Now;
+                Mps_OCUPACIONES.ESTADO = 1;
+                using (MPS_DB db = new MPS_DB())
+                {
+                    db.MPS_OCUPACIONES.Add(Mps_OCUPACIONES);
+                    db.SaveChanges();
+
+                    object datos = (from c in db.MPS_OCUPACIONES
+                                    where c.ID_OCUPACION == Mps_OCUPACIONES.ID_OCUPACION
+                                    select new
+                                    {
+                                        c.ID_OCUPACION,
+                                        c.NOMBRE_OCUPACION,
+                                        c.DESCRIPCION,
+                                        c.FECHA_CREACION,
+                                        c.FECHA_EDICION,
+                                        c.ESTADO
+                                    }).FirstOrDefault();
+
+                    return new { RESPUESTA = true, TIPO = 0, data = datos };
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new { RESPUESTA = false, TIPO = 1, e.Message };
+            }
+
+        }
+
+        public static object ListaOcupacionesId(int id)
+        {
+            using (MPS_DB db = new MPS_DB())
+            {
+
+
+
+                object[] datos = (from c in db.MPS_OCUPACIONES
+
+                                  where c.ID_OCUPACION == id
+                                  select new
+                                  {
+                                      c.ID_OCUPACION,
+                                      c.NOMBRE_OCUPACION,
+                                      c.DESCRIPCION,
+                                      c.FECHA_CREACION,
+                                      c.FECHA_EDICION,
+                                      c.ESTADO
+
+                                  }).ToArray();
+
+
+
+
+                return new { RESPUESTA = true, TIPO = 0, datos };
+            }
+        }
+
+        public static object ModificarOcupaciones(int ID_OCUPACION, string NOMBRE, string DESCRIPCION, int COD_ESTADO)
+        {
+            try
+            {
+                using (MPS_DB db = new MPS_DB())
+                {
+                    DateTime Fecha = DateTime.Now;
+                    string subQuery = $@"UPDATE MPS_OCUPACIONES SET NOMBRE_OCUPACION = '{ NOMBRE }' , DESCRIPCION = '{DESCRIPCION}' ,FECHA_EDICION='{ Fecha.ToString(Formato) }',
+                                       ESTADO = {COD_ESTADO} where ID_OCUPACION = { ID_OCUPACION } ";
+                    db.Database.ExecuteSqlCommand(subQuery);
+
+                    object datos = (from c in db.MPS_OCUPACIONES
+                                    where c.ID_OCUPACION == ID_OCUPACION
+                                    select new
+                                    {
+                                        c.ID_OCUPACION,
+                                        c.NOMBRE_OCUPACION,
+                                        c.DESCRIPCION,
+                                        c.FECHA_CREACION,
+                                        c.FECHA_EDICION,
+                                        c.ESTADO
+                                    }).FirstOrDefault();
+
+
+
+                    return new { RESPUESTA = true, TIPO = 0, data = datos };
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new { RESPUESTA = false, TIPO = 1, e.Message };
+            }
+
+        }
+        #endregion
+
+        #region Prevision
+        public static object PrevisionLitas()
+        {
+            using (MPS_DB db = new MPS_DB())
+            {
+                object[] datos = (from c in db.MPS_PREVISION
+                                  select new
+                                  {
+                                      c.ID_PREVISION,
+                                      c.NOMBRE_PREVISION,
+                                      c.DESCRIPCION,
+                                      c.FECHA_CREACION,
+                                      c.FECHA_EDICION,
+                                      c.ESTADO
+
+                                  }).ToArray();
+
+
+
+                return new { RESPUESTA = true, TIPO = 0, datos };
+
+            }
+        }
+
+        public static object GuardarPrevision(string NombrePrevision, string Descripcion)
+        {
+            try
+            {
+                MPS_PREVISION Mps_PREVISION = new MPS_PREVISION();
+                Mps_PREVISION.NOMBRE_PREVISION = NombrePrevision;
+                Mps_PREVISION.DESCRIPCION = Descripcion;
+                Mps_PREVISION.FECHA_CREACION = DateTime.Now;
+                Mps_PREVISION.ESTADO = 1;
+                using (MPS_DB db = new MPS_DB())
+                {
+                    db.MPS_PREVISION.Add(Mps_PREVISION);
+                    db.SaveChanges();
+
+                    object datos = (from c in db.MPS_PREVISION
+                                    where c.ID_PREVISION == Mps_PREVISION.ID_PREVISION
+                                    select new
+                                    {
+                                        c.ID_PREVISION,
+                                        c.NOMBRE_PREVISION,
+                                        c.DESCRIPCION,
+                                        c.FECHA_CREACION,
+                                        c.FECHA_EDICION,
+                                        c.ESTADO
+                                    }).FirstOrDefault();
+
+                    return new { RESPUESTA = true, TIPO = 0, data = datos };
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new { RESPUESTA = false, TIPO = 1, e.Message };
+            }
+
+        }
+
+        public static object ListaPrevisionId(int id)
+        {
+            using (MPS_DB db = new MPS_DB())
+            {
+
+
+
+                object[] datos = (from c in db.MPS_PREVISION
+
+                                  where c.ID_PREVISION == id
+                                  select new
+                                  {
+                                      c.ID_PREVISION,
+                                      c.NOMBRE_PREVISION,
+                                      c.DESCRIPCION,
+                                      c.FECHA_CREACION,
+                                      c.FECHA_EDICION,
+                                      c.ESTADO
+
+                                  }).ToArray();
+
+
+
+
+                return new { RESPUESTA = true, TIPO = 0, datos };
+            }
+        }
+
+        public static object ModificarPrevision(int ID_PREVISION, string NOMBRE, string DESCRIPCION, int COD_ESTADO)
+        {
+            try
+            {
+                using (MPS_DB db = new MPS_DB())
+                {
+                    DateTime Fecha = DateTime.Now;
+                    string subQuery = $@"UPDATE MPS_PREVISION SET NOMBRE_PREVISION = '{ NOMBRE }' , DESCRIPCION = '{DESCRIPCION}' ,FECHA_EDICION='{ Fecha.ToString(Formato) }',
+                                       ESTADO = {COD_ESTADO} where ID_PREVISION = { ID_PREVISION } ";
+                    db.Database.ExecuteSqlCommand(subQuery);
+
+                    object datos = (from c in db.MPS_PREVISION
+                                    where c.ID_PREVISION == ID_PREVISION
+                                    select new
+                                    {
+                                        c.ID_PREVISION,
+                                        c.NOMBRE_PREVISION,
+                                        c.DESCRIPCION,
+                                        c.FECHA_CREACION,
+                                        c.FECHA_EDICION,
+                                        c.ESTADO
+                                    }).FirstOrDefault();
+
+
+
+                    return new { RESPUESTA = true, TIPO = 0, data = datos };
+                }
+
+            }
+            catch (Exception e)
+            {
+                return new { RESPUESTA = false, TIPO = 1, e.Message };
+            }
+
+        }
+        #endregion
+    }
+}
