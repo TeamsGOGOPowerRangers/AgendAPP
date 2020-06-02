c3fd0c78558a271f3a6093d08cce722cac66fe19
joshef <desingjz@gmail.com>
2020-06-02 17:16:27 -0400

    controladores nuevos

---
+++ AGENDAPP/Controllers/GestionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AGENDAPP.Controllers
+{
+    public class GestionController : Controller
+    {
+        // GET: Gestion
+        public ActionResult Liquidaciones()
+        {
+            return View();
+        }
+        public ActionResult Presupuestos()
+        {
+            return View();
+        }
+        public ActionResult Programas()
+        {
+            return View();
+        }
+        public ActionResult Facturas()
+        {
+            return View();
+        }
+        public ActionResult Cheques()
+        {
+            return View();
+        }
+        public ActionResult Solex()
+        {
+            return View();
+        }
+    }
+}
---
+++ AGENDAPP/Controllers/InformesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AGENDAPP.Controllers
+{
+    public class InformesController : Controller
+    {
+        // GET: Informes
+        public ActionResult Prestaciones()
+        {
+            return View();
+        }
+        public ActionResult Caja()
+        {
+            return View();
+        }
+        public ActionResult Facturacion()
+        {
+            return View();
+        }
+        public ActionResult FacturacionaIsapres()
+        {
+            return View();
+        }
+        public ActionResult Pacientes()
+        {
+            return View();
+        }
+        public ActionResult CajaPrestaciones()
+        {
+            return View();
+        }
+        public ActionResult Ingresos()
+        {
+            return View();
+        }
+        public ActionResult Citas()
+        {
+            return View();
+        }
+    }
+}
